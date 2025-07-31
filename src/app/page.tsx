53d0b64f86aca96dda661ccba92c0d921ca21c39
MSK-101 <msk98.sk@gmail.com>
2025-07-31 18:23:27 +0500

    navigation added for desktop view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FooterSection from "@/components/FooterSection";
 import HeroSection from "@/components/HeroSection";
 import HowWorksSection from "@/components/HowWorksSection";
+import DesktopNavigation from "@/components/navigation/DesktopNavigation";
 
 export default function Home() {
   return (
     <>
+      <DesktopNavigation />
       <HeroSection />
       <HowWorksSection />
       <hr className="md:mx-20" />
---
+++ src/components/navigation/DesktopNavigation.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Button } from "../ui/button";
+
+export function MainNavigationMenu() {
+  return (
+    <NavigationMenu viewport={false}>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>For Agents</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[300px] gap-4">
+              <li>
+                <NavigationMenuLink asChild>
+                  <Link href="#">
+                    <div className="font-medium">Components</div>
+                    <div className="text-muted-foreground">
+                      Browse all components in the library.
+                    </div>
+                  </Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="#">
+                    <div className="font-medium">Documentation</div>
+                    <div className="text-muted-foreground">
+                      Learn how to use the library.
+                    </div>
+                  </Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="#">
+                    <div className="font-medium">Blog</div>
+                    <div className="text-muted-foreground">
+                      Read our latest blog posts.
+                    </div>
+                  </Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>For Providers</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid w-[200px] gap-4">
+              <li>
+                <NavigationMenuLink asChild>
+                  <Link href="#">Components</Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="#">Documentation</Link>
+                </NavigationMenuLink>
+                <NavigationMenuLink asChild>
+                  <Link href="#">Blocks</Link>
+                </NavigationMenuLink>
+              </li>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+            <Link href="#">Features</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+export default function DesktopNavigation() {
+  return (
+    <div className="hidden md:block fixed top-0 left-0 right-0 z-50">
+      <div className="flex justify-between items-center mx-14 rounded-full backdrop-blur-[7.5px] bg-white/10 shadow-[0.75px_3px_10.51px_-2.25px_#5252520F] px-6 py-3 mt-3">
+        <Image src="/logo-dark.png" alt="logo" width={100} height={100} />
+        <MainNavigationMenu />
+        <div>
+          <Button className="bg-transparent text-gray-600 font-semibold rounded-full h-[20px] px-[20px] hover:bg-transparent hover:text-gray-600 cursor-pointer shadow-none">
+            Login
+          </Button>
+          <Button className="bg-brand-900 text-white font-semibold rounded-full h-[48px] px-[20px] hover:bg-brand-900 hover:text-white cursor-pointer">
+            Signup
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
